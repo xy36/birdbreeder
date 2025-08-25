d95ee4ea0a7075b804915382926a8bbfc56e47d6
c.dobiat <c.dobiat@grimme.de>
2025-08-25 14:58:32 +0200

    feat: This is a test

--- lib/features/account/account_page.dart
+++ lib/features/account/account_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:birdbreeder/features/account/account_screen.dart';
 import 'package:flutter/material.dart';
-
-import 'account_screen.dart';
 
 @RoutePage()
 class AccountPage extends StatelessWidget {
@@ -9,6 +8,6 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AccountScreen();
+    return const AccountScreen();
   }
 }
